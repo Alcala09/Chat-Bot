08706011cf64e04b008066dd8034e6a390da5612
Alcala09 <72286166+Alcala09@users.noreply.github.com>
2024-01-08 21:17:03 +0000

    added test chat function to detect keywords, keywords can be found in a sentence
    basically the title

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    //code
+    chatbot bot = new chatbot();
+    Scanner reader = new Scanner(System.in);
+    while (true)
+    {
+      System.out.print("Chat: ");
+      String input = reader.nextLine();
+      System.out.println(bot.detectKeyword(input));
+    }
   }
 }
--- chatbot.java
+++ chatbot.java
@@ -1,31 +1,45 @@
-import java.util.ArrayList;
-class Chatbot {
+import java.util.Arrays;
+
+class chatbot {
 
   private String[] keywordTypes = {"familyKeywords", "petsKeywords", "foodKeywords", "sportsKeywords", "musicKeywords"};
-  
-  private String[] familyKeywords = {"brother", "mother", "father", "sister"};
-  private String[] petsKeywords = {"cat", "dog", "fish", "hamster"};
-  private String[] foodKeywords = {"pizza", "burger", "taco", "salad"};
-  private String[] sportsKeywords = {"soccer", "basketball", "football", "baseball"};
-  private String[] musicKeywords = {"pop", "rock", "hip hop", "country"};
-                                    
-  public Chatbot() 
+
+  private String[][] allKeywords = {
+    {"brother", "mother", "father", "sister"},
+    {"cat", "dog", "fish", "hamster"},
+    {"pizza", "burger", "taco", "salad"},
+    {"soccer", "basketball", "football", "baseball"},
+    {"pop", "rock", "hiphop", "country"}
+  };
+
+  public chatbot() 
   {
     // constructor code here
   }
 
-  public string detectKeyword(string text)
+  private boolean containsKeyword(String inputText, String[] keywords) 
   {
-    inputText = text.toLoweCase.replaceAll("\\s",""); // turns input to lowercase no spaces
+      for (String keyword : keywords) 
+      {
+          if (inputText.contains(keyword)) 
+          {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  public String detectKeyword(String text)
+  {
+    String inputText = text.replaceAll("[^a-zA-Z0-9\\s]", ""); // removes special characters & spaces
 
     for (int i = 0; i < keywordTypes.length; i++) 
     {
-      if (Arrays.asList(keywordTypes[i]).contains(inputText));
+      if (containsKeyword(inputText, allKeywords[i]))
       {
         return keywordTypes[i];
       }
-      
     }
-    
+    return "none";
   }
 }
