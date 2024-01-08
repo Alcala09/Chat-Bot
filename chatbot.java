3dbd9ab9964ddfef9b8ed92f0778cf7cf2d3209b
Alcala09 <72286166+Alcala09@users.noreply.github.com>
2024-01-08 20:26:54 +0000

    basic class setup

---
+++ chatbot.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class Chatbot {
+  
+  public Chatbot() 
+  {
+    // constructor code here
+  }
+
+  public string detectKeyword(string text)
+  {
+    inputText = text.toLoweCase();
+    
+    
+  }
+}
