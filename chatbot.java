a6fcb5330fb2b0d53364713f67393f82b51d2f68
Alcala09 <72286166+Alcala09@users.noreply.github.com>
2024-01-08 20:41:36 +0000

    created keyword list types and method to detect

--- chatbot.java
+++ chatbot.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 class Chatbot {
+
+  private String[] keywordTypes = {"familyKeywords", "petsKeywords", "foodKeywords", "sportsKeywords", "musicKeywords"};
   
+  private String[] familyKeywords = {"brother", "mother", "father", "sister"};
+  private String[] petsKeywords = {"cat", "dog", "fish", "hamster"};
+  private String[] foodKeywords = {"pizza", "burger", "taco", "salad"};
+  private String[] sportsKeywords = {"soccer", "basketball", "football", "baseball"};
+  private String[] musicKeywords = {"pop", "rock", "hip hop", "country"};
+                                    
   public Chatbot() 
   {
     // constructor code here
@@ -8,8 +16,16 @@ class Chatbot {
 
   public string detectKeyword(string text)
   {
-    inputText = text.toLoweCase();
-    
+    inputText = text.toLoweCase.replaceAll("\\s",""); // turns input to lowercase no spaces
+
+    for (int i = 0; i < keywordTypes.length; i++) 
+    {
+      if (Arrays.asList(keywordTypes[i]).contains(inputText));
+      {
+        return keywordTypes[i];
+      }
+      
+    }
     
   }
 }
