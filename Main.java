9251dd0a3c1a62d24f7834199f07cefc60ba1a3e
Alcala09 <72286166+Alcala09@users.noreply.github.com>
2024-01-10 18:15:34 +0000

    made random unknown responses and exit program response

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     chatbot bot = new chatbot("frank");
     Scanner reader = new Scanner(System.in);
-    while (true)
-    {
+    while (true) {
       System.out.print("Chat: ");
       String input = reader.nextLine();
-      
-      System.out.println(bot.respondToKeyword(input)); // temporary to detect type of keyword
+
+      System.out.println(bot.respondToKeyword(input));
     }
   }
 }
--- chatbot.java
+++ chatbot.java
@@ -1,46 +1,56 @@
 import java.util.Arrays;
+
 class chatbot {
-  private String[] keywordTypes = {"family", "pets", "food", "sports", "music", "greeting", "weather", "time"};
+
+  private String[] keywordTypes = { "family", "pets", "food", "sports", "music", "greeting", "weather", "time",
+      "goodbye" };
+
+  private String[] unknownResponse = { "huh?", "I don't know what you mean", "I'm not sure what that is",
+      "I don't understand", "I don't know" };
 
   private String[][] allKeywords = // keywords for the chatbot to recognize
-  {
-    {"brother ", "mother ", "father ", "sister ", "grandpa ", "grandma "}, // family 
-    {"cat ", "dog ", "fish ", "hamster ", "armadillo ", "shark ", "ferret "},  // pet
-    {"pizza ", "burger ", "taco ", "salad ", "burrito ", "beef ", "chicken "}, // food
-    {"soccer ", "basketball ", "football ", "baseball ", "wrestling ", "golf "},  // sports
-    {"pop ", "rock ", "hiphop ", "country ", "song ", "rap ", "jazz "}, // music
-    {"hello ", "hi ", "hey ", "good morning ", "good afternoon ", "good evening"}, // greeting
-    {"weather ", "temperature ", "humidity ", "wind ", "rain ", "snow ", "thunderstorm ", "hurricane"}, // weather
-    {"time ", "date ", "day ", "month ", "year "}, // time
-  };
+      {
+          { "brother ", "mother ", "father ", "sister ", "grandpa ", "grandma " }, // family
+          { "cat ", "dog ", "fish ", "hamster ", "armadillo ", "shark ", "ferret " }, // pet
+          { "pizza ", "burger ", "taco ", "salad ", "burrito ", "beef ", "chicken " }, // food
+          { "soccer ", "basketball ", "football ", "baseball ", "wrestling ", "golf " }, // sports
+          { "pop ", "rock ", "hiphop ", "country ", "song ", "rap ", "jazz " }, // music
+          { "hello ", "hi ", "hey ", "good morning ", "good afternoon ", "good evening" }, // greeting
+          { "weather ", "temperature ", "humidity ", "wind ", "rain ", "snow ", "thunderstorm ", "hurricane" }, // weather
+          { "time ", "date ", "day ", "month ", "year " }, // time
+          { "bye ", "goodbye ", "see you ", "see ya ", "see you later ", "see you" }
+      }; // goodbye;
 
   private String[][] allResponses = // responses for the chatbot to give based on keyword
-  {
-    {"Tell me more about your family.", "How is your family doing?", "What do you do for fun with your family?"}, // family
-    {"Tell me more about your pets.", "What is your favorite pet?", "Do you have any pets?"}, // pet
-    {"Tell me more about your favorite food.", "What is your favorite food?", "Do you like to eat?"}, // food
-    {"Tell me more about your favorite sport.", "What is your favorite sport?", "Do you play any sports?"}, // sport
-    {"Tell me more about your favorite music.", "What is your favorite music?", "Do you like to listen to music?"}, // music
-    {"Hello, how are you?", "Hi, how are you?", "Hey, how are you?", "yo", "what's up?"}, // greeting
-    {"What is the weather like today?", "What is the temperature today?", "What is the humidity today?", "I don't really care about weather"}, // weather
-    {"What is the time?", "What is the date?", "What is the day?", "What is the month?", "I don't know what time it is"}, // weather
-  };
+      {
+          { "Tell me more about your family.", "How is your family doing?",
+              "What do you do for fun with your family?" }, // family
+          { "Tell me more about your pets.", "What is your favorite pet?", "Do you have any pets?" }, // pet
+          { "Tell me more about your favorite food.", "What is your favorite food?", "Do you like to eat?" }, // food
+          { "Tell me more about your favorite sport.", "What is your favorite sport?", "Do you play any sports?" }, // sport
+          { "Tell me more about your favorite music.", "What is your favorite music?",
+              "Do you like to listen to music?" }, // music
+          { "Hello, how are you?", "Hi, how are you?", "Hey, how are you?", "yo", "what's up?" }, // greeting
+          { "What is the weather like today?", "What is the temperature today?", "What is the humidity today?",
+              "I don't really care about weather" }, // weather
+          { "What is the time?", "What is the date?", "What is the day?", "What is the month?",
+              "I don't know what time it is" }, // weather
+          { "Bye", "Goodbye", "See you later", "See you", "See you soon" } // goodbye
+      };
 
-  public chatbot(String name) 
-  {
+  public chatbot(String name) {
     System.out.println("Chatbot initiated, my name is " + name + ".");
   }
 
-  private boolean containsKeyword(String inputText, String[] keywords) // checks if string contains any keyword in keywordtype
+  private boolean containsKeyword(String inputText, String[] keywords) // checks if string contains any keyword in
+                                                                       // keywordtype
   {
-      for (String keyword : keywords) 
-      {
-          if (inputText.contains(keyword)) 
-          {
-              return true;
-          }
+    for (String keyword : keywords) {
+      if (inputText.contains(keyword)) {
+        return true;
       }
-      return false;
+    }
+    return false;
   }
 
   public String detectKeyword(String text) // returns keyword type detected from text
@@ -49,24 +59,25 @@ class chatbot {
     inputText += " ";
     for (int i = 0; i < keywordTypes.length; i++) // checks for keyword
     {
-      if (containsKeyword(inputText, allKeywords[i]))
-      {
+      if (containsKeyword(inputText, allKeywords[i])) {
         return keywordTypes[i];
       }
     }
     return "none";
   }
+
   public String respondToKeyword(String text) // Finds text response based on keyword detected
   {
     String selectedKeyword = detectKeyword(text);
     int index = Arrays.asList(keywordTypes).indexOf(selectedKeyword);
-    if (detectKeyword(text).equals("none"))
-      {
-      return "I don't know how to respond to that.";
-      }
-    else
-      {
+
+    if (detectKeyword(text).equals("none")) {
+      return unknownResponse[(int) (Math.random() * unknownResponse.length)];
+    } else if (detectKeyword(text).equals("goodbye")) {
+      System.exit(0);
+      return "null";
+    } else {
       return allResponses[index][(int) (Math.random() * allResponses[index].length)];
-      }
+    }
   }
 }
