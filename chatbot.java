87cc1fc53a1e7c1538149a7436cbfa7cb983fef6
Alcala09 <72286166+Alcala09@users.noreply.github.com>
2024-01-12 05:01:31 +0000

    made simplifying messages cooler

--- chatbot.java
+++ chatbot.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
 class chatbot {
-  private String[][] keywords = {
+  private String[][] keywords = { // simplifies pronouns to 4 basic pronouns
       { // ME STATEMENTS
-          "me", "i", "my", "myself", "we", "us", "i am"
+          "me", "i", "my", "myself", "we", "us"
       },
       { // YOU STATEMENTS
           "you", "your", "yourself"
@@ -15,15 +15,19 @@ class chatbot {
           "they", "their", "theyre", "person", "he", "she", "them", "themself"
       },
   };
+  private String[] removedWords = { // words removed in input to make it easier to process
+      "am", "is", "are", "was", "were", "be", "been", "being", "the"
+  };
 
   public chatbot(String name) {
     System.out.println("Chatbot initiated, my name is " + name + ".");
   }
 
-  public String detectMessageType(String input) {
+  public String detectMessageType(String input) { // find one of 4 pronoun types
     for (int i = 0; i < keywords.length; i++) {
       for (int j = 0; j < keywords[i].length; j++) {
-        if (input.contains(keywords[i][j])) {
+        String currentKeyword = keywords[i][j] + " ";
+        if (input.contains(currentKeyword)) {
           return keywords[i][0];
         }
       }
@@ -32,24 +36,28 @@ class chatbot {
   }
 
   public String simplifyInput(String originalInput) // makes input code-readable
-  {
-    String input = originalInput.toLowerCase();
+  { // clean up original input
+    String input = originalInput.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
+
+    for (int i = 0; i < removedWords.length; i++) { // remove words from removedWords list
+      input = input.replace(removedWords[i] + " ", "");
+    }
+
+    // get message pronoun type
     String messageType = detectMessageType(input);
-    
+
     int index = -1;
-    for (int i = 0; i < keywords.length; i++) 
-    {
-      if (Arrays.asList(keywords[i]).contains(messageType))
-      {
+    for (int i = 0; i < keywords.length; i++) { // find keyword that corresponds with pronoun type
+      if (Arrays.asList(keywords[i]).contains(messageType)) {
         index = i;
         break;
       }
     }
-      for (int j = 0; j < keywords[index].length; j++) 
-        {
-          input = input.replace(keywords[index][j], keywords[index][0]);
-        }
-
+    if (index != -1) { // simplifies pronoun to most basic form
+      for (int j = 0; j < keywords[index].length; j++) {
+        input = input.replace(keywords[index][j] + " ", keywords[index][0] + " ");
+      }
+    }
     return input;
   }
   // other function
